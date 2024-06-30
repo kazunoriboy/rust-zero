694d3c1b0036aaa4c36f4a13197d3fa88875a144
kazunoriboy <matunorigmnw@gmail.com>
2024-06-30 15:38:24 +0900

    列挙型：同じ種類のデータのオプションを作る時などに使用するとよさそう

--- work/hello/src/main.rs
+++ work/hello/src/main.rs
@@ -1,16 +1,9 @@
-fn do_it(f: fn(u32, u32) -> u32, a: u32, b: u32) {
-    println!("{}", f(a, b));
-}
-
-fn add(a: u32, b: u32) -> u32 {
-    a + b
-}
-
-fn mul(a: u32, b: u32) -> u32 {
-    a * b
-}
-
 fn main() {
-    do_it(add, 10, 2);
-    do_it(mul, 10, 2);
+    enum Storage {
+        HDD { size: u32, rpm: u32 },
+        SSD(u32),
+    }
+
+    let hdd = Storage::HDD { size: 512, rpm: 7200 };
+    let ssd = Storage::SSD(512);
 }
