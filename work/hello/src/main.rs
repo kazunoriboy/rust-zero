2716bd68e76a100f65fe6e29cd723602f486aca0
kazunoriboy <matunorigmnw@gmail.com>
2024-07-11 01:21:38 +0900

    複雑なマッチ式を記述

--- work/hello/src/main.rs
+++ work/hello/src/main.rs
@@ -1,13 +1,41 @@
 fn main() {
-    let v = [3, 8, 11, 15];
-    let mut result = 0;
-    for x in v.iter() {
-        println!("x: {}", *x);
-        if *x % 2 == 0 {
-            continue;
-        }
-        result += *x;
+    #[derive(Debug)]
+    struct PCSpec {
+        cpus: u16,
+        memory: u32,
+        storage: Storage,
     }
-    println!("Result: {}", result);
+    #[derive(Debug)]
+    enum Storage {
+        HDD { size: u32, rpm: u32 },
+        SSD(u32),
+    }
+    let spec = PCSpec {
+        cpus: 4,
+        memory: 16,
+        storage: Storage::SSD(512),
+    };
+
+    match &spec {
+        PCSpec {
+            storage: Storage::SSD(512),
+            ..
+        } => {
+            println!("You have a 512GB SSD");
+        }
+        PCSpec {
+            cpus: 4 | 8,
+            memory: m,
+            storage: _,
+        } => {
+            println!("4 or 8 CPUs");
+            println!("Memory: {} GB", *m);
+        }
+        PCSpec { memory: m, .. } if *m < 4 => {
+            println!("4GiB or less memory");
+        }
+        _ => ()
+    }
+
 }
 
