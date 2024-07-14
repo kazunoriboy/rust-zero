ceca8cb1d1256a91d9e9fcb82de5b313652f1d26
kazunoriboy <matunorigmnw@gmail.com>
2024-07-14 18:19:24 +0900

    returnを使うと値を返して関数の実行を終了する

--- work/hello/src/main.rs
+++ work/hello/src/main.rs
@@ -1,41 +1,14 @@
 fn main() {
-    #[derive(Debug)]
-    struct PCSpec {
-        cpus: u16,
-        memory: u32,
-        storage: Storage,
-    }
-    #[derive(Debug)]
-    enum Storage {
-        HDD { size: u32, rpm: u32 },
-        SSD(u32),
-    }
-    let spec = PCSpec {
-        cpus: 4,
-        memory: 16,
-        storage: Storage::SSD(512),
-    };
+    fn average(v: &[f32]) -> Option<f32> {
+        if v.is_empty() {
+            return None;
+        }
+        let mut total = 0.0;
+        for n in v {
+            total += n;
+        }
 
-    match &spec {
-        PCSpec {
-            storage: Storage::SSD(512),
-            ..
-        } => {
-            println!("You have a 512GB SSD");
-        }
-        PCSpec {
-            cpus: 4 | 8,
-            memory: m,
-            storage: _,
-        } => {
-            println!("4 or 8 CPUs");
-            println!("Memory: {} GB", *m);
-        }
-        PCSpec { memory: m, .. } if *m < 4 => {
-            println!("4GiB or less memory");
-        }
-        _ => ()
+        Some(total / v.len() as f32)
     }
-
 }
 
