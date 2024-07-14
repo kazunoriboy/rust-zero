2f06dee0bb49f9520e4ccde9d1352913b3b4e904
kazunoriboy <matunorigmnw@gmail.com>
2024-07-14 18:53:39 +0900

    クロージャを使って関数を定義し、実行する

--- work/hello/src/main.rs
+++ work/hello/src/main.rs
@@ -1,14 +1,6 @@
 fn main() {
-    fn average(v: &[f32]) -> Option<f32> {
-        if v.is_empty() {
-            return None;
-        }
-        let mut total = 0.0;
-        for n in v {
-            total += n;
-        }
-
-        Some(total / v.len() as f32)
-    }
+    let f = |a, b| a + b;
+    let n = f(10, 20);
+    println!("{}", n);
 }
 
