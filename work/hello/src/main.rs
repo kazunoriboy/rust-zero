bea25d56b2a2f2ac24cedea7af43e6c34dd33072
kazunoriboy <matunorigmnw@gmail.com>
2024-07-15 11:45:53 +0900

    列挙型のデータにメソッドを定義

--- work/hello/src/main.rs
+++ work/hello/src/main.rs
@@ -1,6 +1,22 @@
 fn main() {
-    let f = |a, b| a + b;
-    let n = f(10, 20);
-    println!("{}", n);
+    enum Storage {
+        HDD { size: u32, rpm: u32 },
+        SSD(u32),
+    }
+    impl Storage {
+        fn get_size(&self) -> u32 {
+            match self {
+                Storage::HDD { size: s, .. } => *s,
+                Storage::SSD(s) => *s,
+            }
+        }
+
+        fn set_size(&mut self, size: u32) {
+            match self {
+                Storage::HDD { size: s, .. } => *s = size,
+                Storage::SSD(s) => *s = size,
+            }
+        }
+    }
 }
 
