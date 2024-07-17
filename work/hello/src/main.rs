936acabe92cb275188c0bba517ea6d2e64bc27c9
kazunoriboy <matunorigmnw@gmail.com>
2024-07-18 00:23:56 +0900

    channelを使ってスレッド間でデータの受け渡しをする

--- work/hello/src/main.rs
+++ work/hello/src/main.rs
@@ -1,15 +1,18 @@
 fn main() {
-    fn worker() -> u32 {
-        println!("worker");
-        100
+    let (tx, rx) = std::sync::mpsc::sync_channel(64);
+
+    let handler = std::thread::spawn(move || match rx.recv() {
+        Ok((x, y)) => println!("({}, {})", x, y),
+        Err(e) => eprintln!("{e}"),
+    });
+
+    if let Err(e) = tx.send((10, 20)) {
+        eprintln!("{e}");
     }
 
-    let handler = std::thread::spawn(worker);
-
-    match handler.join() {
-        Ok(n) => println!("{n}"),
-        Err(e) => println!("{:?}", e),
+    if let Err(e) = handler.join() {
+        eprintln!("{:?}", e);
     }
-
+    
 }
 
