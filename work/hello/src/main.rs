1e4a52d37f9a9ce309d09fa6f79f699eaec14353
kazunoriboy <matunorigmnw@gmail.com>
2024-07-17 23:45:54 +0900

    どのような繰り返し処理なのかの意図が明確なメソッドの使用例

--- work/hello/src/main.rs
+++ work/hello/src/main.rs
@@ -1,14 +1,27 @@
 use std::collections::BTreeSet;
+use std::collections::LinkedList;
 
 fn main() {
+    let mut v = Vec::new();
+    v.push(10);
+    v.push(5);
+
     let mut s = BTreeSet::new();
     s.insert(100);
     s.insert(400);
-    s.insert(6);
-    s.insert(1);
 
-    for n in s.iter() {
+    let it = v.iter().chain(s.iter());
+    for n in it.clone().map(|n| n * 2) {
         println!("{}", n);
+    }
+
+    let total = it.clone().fold(0, |acc, x| acc + x);
+    println!("Total: {}", total);
+
+    let list: LinkedList<_> = it.filter(|n| *n % 2 == 0).collect();
+
+    for (n, m) in v.iter().zip(s.iter()) {
+        println!("{}, {}", n, m);
     }
 }
 
