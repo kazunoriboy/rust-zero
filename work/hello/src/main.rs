6e48191ce68a1aa0f59fbc5fa7e3413f77805b4c
kazunoriboy <matunorigmnw@gmail.com>
2024-07-25 00:02:12 +0900

    ライフタイム指定子を使うと参照が有効であることを保証し、メモリ安全性を確保できるらしい、、、？

--- work/hello/src/main.rs
+++ work/hello/src/main.rs
@@ -1,9 +1,21 @@
 fn main() {
-    let a;
-    {
-        let b = 10;
-        a = &b;
-        println!("{}", a);
+    let a: i32 = 10;
+    let p: i32 = 6;
+    let b: &i32 = &p;
+
+    fn square<'a>(x: &'a i32) -> i32 {
+        x * x
     }
+
+    let res = square(b);
+    println!("res: {}", res);
+
+    #[derive(Debug)]
+    struct Foo<'a> {
+        x: &'a i32,
+    }
+
+    let foo = Foo { x: &a };
+    println!("{:?}", foo);
 }
 
