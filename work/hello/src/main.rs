2fba8cc3692fd0cf472a1c0fb2680496211dd021
kazunoriboy <matunorigmnw@gmail.com>
2024-07-24 23:08:18 +0900

    変数が代入で消費される例

--- work/hello/src/main.rs
+++ work/hello/src/main.rs
@@ -1,21 +1,11 @@
 fn main() {
-    #[derive(Debug)]
-    struct H2O {}
-    struct O2 {}
-    struct H2 {}
+    struct Coin {}
 
-    fn burn(_h2_1: H2, _h2_2: H2, _o2: O2) -> (H2O, H2O) {
-        (H2O {}, H2O {})
-    }
+    let a = Coin {};
+    let b = a;
+    let c = b;
 
-    let h2_1 = H2 {};
-    let h2_2 = H2 {};
-    let o2 = O2 {};
-
-    let (h2o_1, h2o_2) = burn(h2_1, h2_2, o2);
-
-    // 以下のコードをコメントインするとコンパイルエラーになる
-    // let (h2o_1, h2o_2) = burn(h2_1, h2_2, o2);
-    println!("{:?} {:?}", h2o_1, h2o_2);
+    // 以下はすでに変数の中身を代入して消費しているのでエラーになる
+    // let d = a;
 }
 
